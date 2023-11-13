3006c32c9ac1806664d49b84009de2ef9603023b
lethalfb <lethalfb@gmail.com>
2023-11-13 10:15:56 -0800

    first commi

---
+++ install_tf.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+set -e
+tf_version=0.15.0
+echo "DOWNLOADING TERRAFORM"
+wget https://releases.hashicorp.com/terraform/"$tf_version"/terraform_"$tf_version"_linux_amd64.zip
+unzip terraform_"$tf_version"_linux_amd64.zip
+sudo mv terraform /usr/local/bin/
+rm terraform_"$tf_version"_linux_amd64.zip
